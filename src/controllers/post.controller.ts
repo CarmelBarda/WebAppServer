55cfcbc2d6a35a2420b468833f5814f4559d0356
CarmelBarda <carmelbd78@colman.ac.il>
2024-12-13 13:35:58 +0200

    get post by id

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,4 +1,4 @@
-import { FilterQuery, Model } from "mongoose";
+import mongoose, { FilterQuery, Model } from "mongoose";
 import { BaseController } from "./base.controller";
 import { IPost } from "../models/interfaces/IPost";
 import { Request, Response } from "express";
@@ -18,6 +18,20 @@ export class PostController extends BaseController<IPost> {
     getAllPosts = async (_: Request, res: Response) => {
         const allPosts = await this.getPostsByFilter({});
         res.status(200).send(allPosts);
+    }
+
+    getPostById = async (req: Request, res: Response) => {
+        const postId = req.params.id;
+
+        if (!mongoose.Types.ObjectId.isValid(postId)) {
+            return res.status(400).send({ error: "ownerId isn't valid" });
+        }
+
+        const post = await this.getPostsByFilter({ 
+            _id: new mongoose.Types.ObjectId(postId) 
+        });
+        
+        res.status(200).send(post);
     }
 };
 
--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.get('/', postController.getAllPosts);
 
+router.get('/:id', postController.getPostById);
+
 export default router;
