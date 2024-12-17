44495ab72db3c4d0f0f29b039a90a48ada8b2f92
Aya <ayatal.712@cs.colman.ac.il>
2024-12-17 22:04:45 +0200

    comments request routes

--- src/routes/comment.route.ts
+++ src/routes/comment.route.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import postController from "../controllers/post.controller";
+import { commentController } from "../controllers/comment.controller";
 
 const router = express.Router();
 
-router.get('/', postController.getAllPosts);
+router.post('/', commentController.createComment);
+router.put('/:id', commentController.updateComment);
+router.post('/:post', commentController.getPostComments);
 
 export default router;
