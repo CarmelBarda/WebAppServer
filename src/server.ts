0e69cf3eb87c4e0bf9bd320d431a9ad7130fc64b
Aya <ayatal.712@cs.colman.ac.il>
2025-01-02 15:49:43 +0200

    remove server settings

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,9 @@
 import createServer from "./app";
 import http, { Server } from "http";
-import swaggerUI from "swagger-ui-express";
-import swaggerJsDoc from "swagger-jsdoc";
 
 const port: string = process.env.PORT;
 
 createServer().then((app) => {
-
-  const options = {
-    definition: {
-      openapi: "3.0.0",
-      info: {
-        title: "Web Advanced Application development 2023 REST API",
-        version: "1.0.1",
-        description:
-          "REST server",
-      },
-      servers: [
-        {
-          url:  "http://localhost:" + port
-        },
-      ],
-    },
-    apis: ["./src/routes/*.ts"],
-  };
-
-  const specs = swaggerJsDoc(options);
-  
-  app.use("/api", swaggerUI.serve, swaggerUI.setup(specs));
   let server: Server;
   server = http.createServer(app);
 
