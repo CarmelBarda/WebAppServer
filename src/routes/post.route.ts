5973b9ca9777359e2903905b14f2c047afc0e0e5
Aya <ayatal.712@cs.colman.ac.il>
2025-01-03 23:30:04 +0200

    added middlware on post api

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -1,17 +1,18 @@
 import express from "express";
 import postController from "../controllers/post.controller";
+import authMiddleware from "../commons/middlewares/auth";
 
 const router = express.Router();
 
-router.get('/:id', postController.getPostById);
-router.get('/', (req, res) => {
+router.get('/:id', authMiddleware, postController.getPostById);
+router.get('/', authMiddleware, (req, res) => {
     if (req.query.sender) {
         return postController.getPostsBySender(req, res);
     }
     
     return postController.getAllPosts(req, res);
 });
-router.post('/', postController.createPost);
-router.put('/:id', postController.updatePost);
+router.post('/', authMiddleware, postController.createPost);
+router.put('/:id', authMiddleware, postController.updatePost);
 
 export default router;
