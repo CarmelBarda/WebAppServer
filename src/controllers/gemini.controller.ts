8b33908c6a5f7162766145783cb6cecea5eb1ea4
CarmelBarda <carmelbd78@colman.ac.il>
2025-02-02 19:46:45 +0200

    add tests

--- src/controllers/gemini.controller.ts
+++ src/controllers/gemini.controller.ts
@@ -2,11 +2,15 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 import { Request, Response } from 'express';
 
 export const sendGeminiReq = async (req: Request, res: Response) => {
-  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-  const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+  try {
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
 
-  const prompt = req.body.prompt;
-  const result = await model.generateContent(prompt);
+    const prompt = req.body.prompt;
+    const result = await model.generateContent(prompt);
 
-  res.status(200).send(result.response.text());
+    res.status(200).send(result.response.text());
+  } catch (err) {
+    res.status(500).json({ message: 'Gemini Request Failed.' });
+  }
 };
