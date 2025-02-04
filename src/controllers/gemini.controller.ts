6dd01c18cf499883f951948d438e639b1ce299fb
CarmelBarda <carmelbd78@colman.ac.il>
2025-02-05 00:33:56 +0200

    prompt fix

--- src/controllers/gemini.controller.ts
+++ src/controllers/gemini.controller.ts
@@ -6,10 +6,9 @@ export const sendGeminiReq = async (req: Request, res: Response) => {
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
     const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
 
-    const bookName = req.query.bookName;
-    const prompt = `I have read and liked the book: ${bookName}. 
+    const prompt = `I have read and liked the book: ${req.query.bookName}. 
     I want you to give me a reccomentation for my next book based on this book.
-    Please return an json answer with fields: title, author, and description.
+    Please return a json answer with fields: title, author, and description.
     Please return the response without the word json at the begginning.`;
 
     const result = await model.generateContent(prompt);
@@ -17,6 +16,7 @@ export const sendGeminiReq = async (req: Request, res: Response) => {
 
     res.status(200).json(json);
   } catch (err) {
+    console.error(err);
     res.status(500).json({ message: 'Gemini Request Failed.' });
   }
 };
